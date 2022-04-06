7a5e254a0a641f167833eb0d2483c87705c041b9
superRaptor911 <raptor.inc2018@gmail.com>
2022-04-06 20:44:02 +0530

    gg

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,6 @@ const Home = () => {
         <Link to="login">Captain Login</Link>
         <Link to="register">Captain Register(admin)</Link>
         <Link to="eventcreate">Create event(admin)</Link>
-        <Link to="eventregister">event register</Link>
         <Link to="eventupdate">Update event(admin)</Link>
         <Link to="events">Events</Link>
         <Link to={ROUTES.eventsViewer}>Events Viewer</Link>
